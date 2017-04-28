00012111a1afde3bf86bbc089faf3790b69f7cb3
cancel.wu <cancel.wu@foxmail.com>
2017-04-29 01:51:52 +0800

    自动回复

--- routes/weixin.js
+++ routes/weixin.js
@@ -1,34 +1,63 @@
 const express = require('express');
 const router = express.Router();
 const crypto = require('crypto');
+const wechat = require('wechat');
+const WechatAPI = require('wechat-api');
+const _ = require('lodash');
 
-const weixinConfig = {
-  id: 'wx718b667be9b708b2',
-  secret: 'd61cc9e73723f413ce3af3d2ebfe202a',
-  token: 'CAIDAMAODEXIAODANGAO',
-  encodingAESKey: 'aawOE2hSzsniM43AAU2WsZbEEWRMJEzSe33Dy0Jcy8a',
-  checkSignature: true
+module.exports = function (app) {
+  const weixinConfig = {
+    appid: 'wx718b667be9b708b2',
+    secret: 'd61cc9e73723f413ce3af3d2ebfe202a',
+    token: 'CAIDAMAODEXIAODANGAO',
+    encodingAESKey: 'aawOE2hSzsniM43AAU2WsZbEEWRMJEzSe33Dy0Jcy8a',
+    checkSignature: false
+  };
+
+  const api = new WechatAPI(weixinConfig.appid, weixinConfig.secret);
+
+  app.use('/wxapi', wechat(weixinConfig)
+    .text(function(message, req, res, next) {
+      console.log(message);
+      res.reply("hehe")
+    })
+    .event(function(message, req, res, next) {
+      switch(message.Event) {
+        case "SCAN":
+        case "subscribe":
+          // if(_.get(message, 'EventKey')) {
+          //   res.reply("event subscribe saoma");
+          // }else {
+            api.getMaterialCount((err, result, response) => {
+              const offset = _.get(result, 'news_count') - 1;
+              console.log(offset);
+              api.getMaterials('news', offset, 1, (err, news) => {
+                res.reply([
+                  {
+                    title: _.get(news, 'item.0.content.news_item.0.title'),
+                    description: _.get(news, 'item.0.content.news_item.0.digest'),
+                    picurl: _.get(news, 'item.0.content.news_item.0.thumb_url'),
+                    url: _.get(news, 'item.0.content.news_item.0.url')
+                  }
+                ])
+              })
+            })
+          //}
+          break;
+        case "unsubscribe":
+          res.reply("event unsubscribe");
+          break;
+        case "LOCATION":
+          res.reply("event LOCATION");
+          break;
+        case "CLICK":
+          //res.reply("event caidan", _.get(message, 'EventKey'));
+          break;
+        case 'VIEW':
+          //res.reply("event view", _.get(message, 'EventKey'));
+      }
+    })
+    .middlewarify()
+  );
+
 };
-
-router.get('/', function (req, res, next) {
-
-  try {
-    const params = req.query;
-    const TOKEN = weixinConfig.token;
-    const signature = params.signature;
-    const timestamp = params.timestamp;
-    const nonce = params.nonce;
-    const echostr = params.echostr;
-    const tmpArr = [TOKEN, timestamp, nonce];
-    const hash = crypto.createHash('sha1');
-    const tmpStr = hash.update(tmpArr.sort().join('')).digest('hex');
-    if (tmpStr === signature) {
-      res.send(echostr);
-    } else {
-      res.send(false);
-    }
-  } catch (e) { res.send(e); }
-});
-
-
-module.exports = router;
